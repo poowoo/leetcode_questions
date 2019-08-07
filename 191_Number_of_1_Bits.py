66775db659d8da439810e6e47f3637041b6b8b47
poowoo <c2333391@gmail.com>
2019-08-07 15:08:24 +0800

    20 ms, faster than 54.68% of submissions

--- 191_Number_of_1_Bits.py
+++ 191_Number_of_1_Bits.py
@@ -6,7 +6,7 @@ class Solution(object):
         """                
         sum = 0
         while(n >= 1):
-            if int(n) % 2 == 1:
+            if int(n) & 1 == 1:
                 sum += 1
             n /= 2                
         return sum
